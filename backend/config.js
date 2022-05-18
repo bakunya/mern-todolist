8a3fc83bab93086f2954b86aba159872bda17dda
bakunya <bakunya@nyaan.kawaii>
2022-05-18 19:57:22 +0700

    init

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    port: 8000,
-    host: 'localhost',
-    mongoURI: 'mongodb+srv://bakunya:V5UxNogFuTRBKRQR@cluster0.hymw2.mongodb.net/todolist?retryWrites=true&w=majority',
-    JWTSecretKey: 'jwt-secret-random-string',
-    JWTExpires: 60 * 60 * 24 * 3 // 3 days
-}
-
-module.exports = config
--- frontend/src/config.js
+++
@@ -1,2 +0,0 @@
-export const BASEURL = 'https://todolist-mern-backend.herokuapp.com'
-export const BASEURL_API = `${BASEURL}/v1`
